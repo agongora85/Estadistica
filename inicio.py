b09019fdd9f089c626b098f430c40058a0dc20ea
agongora85 <141359860+agongora85@users.noreply.github.com>
2024-12-05 10:05:29 -0600

    Primera actualización

---
+++ inicio.py
@@ -0,0 +1,11 @@
+import pandas as pd
+#Definimos una serie simple numérica
+serie=pd.Series([1,2,3])
+print(serie)
+#Definimos una serie de tipo cadena o texto
+s = pd.Series(['Matemáticas', 'Historia', 'Economía', 'Programación', 'Inglés'], dtype='string')
+print(s)
+serie.name='nombre'
+#Definimos una serie con columnas
+df=pd.DataFrame({'Columna 1':[1,2,3,4],'Columna 2':['a','b','c','d']})
+print(df)
