627d3b42649351a36e5cc2b9d3478a0bbcb5ff85
agongora85 <agongoraescalante@gmail.com>
2024-12-06 20:08:56 -0600

    Actualizacion 08:08 pm

--- inicio.py
+++ inicio.py
@@ -1,4 +1,9 @@
+#Definimos los módulos a importar
 import pandas as pd
+#Definimos las funciones a utilizar
+from statistics import median,mean
+from math import isnan
+from itertools import filterfalse
 #Definimos una serie simple numérica
 serie=pd.Series([1,2,3])
 print(serie)
@@ -10,3 +15,9 @@ serie.name='nombre'
 df=pd.DataFrame({'Columna 1':[1,2,3,4],'Columna 2':['a','b','c','d']})
 #Esto es una prueba para actualizar
 print(df)
+data=[20.7,15,63.25,142.14]
+print(sorted(data))
+print(sorted(data,key=None,reverse=True))
+print(median(data)) #Para obtener la mediana
+print(sum(data)) #Obtener la sumatoria
+print(mean(data))
