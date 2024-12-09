c37578abccccdca78cbcdc6ddff998f410c3f58e
agongora85 <141359860+agongora85@users.noreply.github.com>
2024-12-09 12:26:58 -0600

    Actualización a las 12:26 pm lunes 09 de diciembre

--- inicio.py
+++ inicio.py
@@ -3,14 +3,10 @@ import pandas as pd
 #Definimos las funciones a utilizar
 from statistics import median,mean,mode,multimode
 from math import isnan,floor
-from itertools import filterfalse
-import openpyxl
-from tabulate import tabulate
 #Definimos una serie simple numérica
 serie=pd.Series([1,2,3])
 print(serie)
 print("La media es:"+str(median(serie)))
-
 #Definimos una serie de tipo cadena o texto
 s = pd.Series(['Matemáticas', 'Historia', 'Economía', 'Programación', 'Inglés'], dtype='string')
 print(s)
@@ -21,29 +17,40 @@ print(sorted(s))
 df=pd.DataFrame({'Columna 1':[1,2,3,4],'Columna 2':['a','b','c','d']})
 #Esto es una prueba para actualizar
 print(df)
-data=[20.7,15,63.25,142.14]
-data2=[10,52,10,63,87,63,10,63]
-print(sorted(data))
-print(sorted(data,key=None,reverse=True))
-print(median(data)) #Para obtener la mediana
-print(sum(data)) #Obtener la sumatoria
-print(floor(mean(data))) #Para obtener la media con rendondeo
-print(mode(data2))
-print(multimode(data2))
+#---------- Trabajo con el archivo de Excel -----------------------
+archivo1='data/Productividad.xlsx'
+#Leemos el archivo de Excel
+a=pd.read_excel(archivo1)
+col=a.columns
+#Obtenemos la lista de las columnas
+col=col.tolist()
+print(col)
+#Imprimimos los valores por fila
+valores=a.values
+#Definimos las variables para obtener los datos estadísticos
+sumpp=0
+tpp=0
+for columna in a:
+    #print(a["Proyectos_planeados"])
+    sumpp=a["Proyectos_planeados"]
 
-#Obtenemos el tamaño de la tabla 
-print(df.shape)
-archivo='data/Resultados.xlsx'
-excel_dataframe=openpyxl.load_workbook(archivo)
-#Conocemos la hoja de calculo que se encuentra activa
-print(excel_dataframe.active)
-data_frame=excel_dataframe.active
-data=[]
-for row in range(1,data_frame.max_row):
-    _row=[row]
-    for col in data_frame.iter_cols(1,data_frame.max_column):
-        #print(col[row].value)
-        _row.append(col[row].value)
-    data.append(_row)
-headers=["Herramienta","Dominio del tema","Participación del grupo","Buena estructura","Claridad en contenido","Ejemplos","Tono de voz", "Tiempo de exposición","Imagenes"]
-print(tabulate(data,headers=headers))
+print(sumpp)
+for b in sumpp:
+    tpp=tpp+b
+print("La sumatoria de los proyectos planeados son:"+str(tpp))
+print("La media de los proyectos planeados es:"+str((round(mean(sumpp),2))))
+print("La sumatoria de los proyectos concretados es: "+str(sum(a["Proyectos concretados"])))
+print("El empleado que tiene más movimientos es: "+str(mode(a["#Credencial"])))
+print("Nivel de avance de todos los proyectos "+str(round((sum(a["Proyectos concretados"])/sum(a["Proyectos_planeados"]))*100))+"%")
+# ------------------------------------------------------------
+#---------------- Trabajo con listados ----------------------
+data2=[20.7,15,63.25,142.14]
+data3=[10,52,10,63,87,63,10,63]
+
+print(sorted(data2))
+print(sorted(data2,key=None,reverse=True))
+print(median(data3)) #Para obtener la mediana
+print(sum(data3)) #Obtener la sumatoria
+print(floor(mean(data3))) #Para obtener la media con rendondeo
+print(mode(data3))
+print(multimode(data3))
